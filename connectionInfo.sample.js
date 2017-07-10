45699e31aff749272233035e757c6ae6215dba62
Lamarius <look.bobbyl@gmail.com>
2017-07-09 21:48:46 -0500

    Added a sample connectionInfo file

---
+++ connectionInfo.sample.js
@@ -0,0 +1,9 @@
+// A sample connectionInfo file, replace all values with values used for your bot, then remove the
+// .sample from the file name
+
+module.exports = {
+  host: 'host',
+  user: 'user',
+  password: 'password',
+  database: 'database'
+}
